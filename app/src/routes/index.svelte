916bc76c64b69a1b5926dde3a9393cf8cde3c5a9
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-06 23:47:05 +0000

    form

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,2 +1,14 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
--- app/svelte.config.js
+++ app/svelte.config.js
@@ -4,17 +4,8 @@ import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
         adapter: adapter({
-          // if true, will deploy the app using edge functions
-          // (https://vercel.com/docs/concepts/functions/edge-functions)
-          // rather than serverless functions
           edge: false,
-    
-          // an array of dependencies that esbuild should treat
-          // as external when bundling functions
-    
-          // if true, will split your app into multiple functions
-          // instead of creating a single one for the entire app
-          split: false
+          split: true,
         }),
         vite: {
            server: {
