ec2ef27e6230e94ba6878a96f04b427be9d8e965
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-06 02:09:23 +0000

    Actual update :)

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,9 +1,9 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: vercel({
+        adapter: adapter({
           // if true, will deploy the app using edge functions
           // (https://vercel.com/docs/concepts/functions/edge-functions)
           // rather than serverless functions
@@ -11,7 +11,6 @@ const config = {
     
           // an array of dependencies that esbuild should treat
           // as external when bundling functions
-          external: [],
     
           // if true, will split your app into multiple functions
           // instead of creating a single one for the entire app
