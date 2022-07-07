a8b27166b939ab32b2421a5eb1250af6c4ba05da
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-07 00:22:41 +0000

    lololol faill

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -2,7 +2,7 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<form on:submit = {handleSubmit} name = "first">
+<form name = "first">
     <input type = "text" name = "name">
    <input type="submit" value="Subscribe!">
 </form>
