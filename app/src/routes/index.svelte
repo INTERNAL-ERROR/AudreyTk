c353748e842818e4ed52bb59fb1a76270526a2c9
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-07 00:13:01 +0000

    lol failllll

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,18 +1,33 @@
-<script context="module">
-  export const prerender = false;
-</script>
+
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+<form on:submit = {handleSubmit} name = "first">
+    <input type = "text" name = "name">
+   <input type="submit" value="Subscribe!">
 </form>
+
+<script>
+    function encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) =>
+            encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    }
+    
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": event.target.getAttribute("name"),
+          ...name,
+        }),
+      })
+        .then(() => navigate("/"))
+        .catch((error) => alert(error));
+    };
+</script>
