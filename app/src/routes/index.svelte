ee52f4b5b2d61a87c424ce817cc565b464a93f6d
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-07 03:20:28 +0000

    I suck

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<form name="test" netlify netlify-honeypot="bot-field" hidden>
+<form name="test" netlify netlify-honeypot="bot-field">
     <input type="text" name="name" />
     <input type="email" name="email" />
     <textarea name="message"></textarea>
