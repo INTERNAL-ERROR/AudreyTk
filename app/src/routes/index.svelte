6ade3a908030cbc75c6d2e0b6e1accc7e17a87cf
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-07-07 03:14:26 +0000

    lol

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,26 +1,11 @@
-
+<script context="module">
+    export const prerender = true;
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<form name = "first">
-    <input type = "text" name = "name">
-   <input type="submit" value="Subscribe!">
+<form name="test" netlify netlify-honeypot="bot-field" hidden>
+    <input type="text" name="name" />
+    <input type="email" name="email" />
+    <textarea name="message"></textarea>
 </form>
-<script>
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let myForm = document.getElementById("pizzaOrder");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-</script>
